32b1ecc7246ef9756663c2f41ed8ca0232f699dc
kishan <kishankarena619@gmail.com>
2021-06-27 01:07:29 +0530

    created new cards for api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,79 +1,89 @@
-import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
-  const [input , setInput] =useState('');
-  const [results , setResults] =useState(null);
-  const [searchOption , setSearchOption] = useState('shows');
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearch = searchOption === 'shows';
- 
-  const onSearch = ()=>{
+
+  const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
-    })
-   
-    };
+    });
+  };
 
-  const onInputChange =(ev)=>{
+  const onInputChange = ev => {
     setInput(ev.target.value);
   };
 
-  const onKeyDown = (ev) => {
-    if(ev.keyCode === 13)
-    {
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
       onSearch();
     }
-    
   };
 
-  const onRadioChange = (ev)=>{
-    setSearchOption(ev.target.value)
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
   };
-  
-  
 
-  const renderResults =()=>{
-    if(results && results.length === 0){
-      return (
-        <div>No results</div>
-      );
-
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
     }
-    if(results && results.length > 0){
-       return( results[0].show ? results.map( item =>(
-        <div key ={item.show.id}>{item.show.name}</div>
-        )) : results.map( item =>(
-          <div key ={item.person.id}>{item.person.name}</div>
-          ))
-            
-             );
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
-  }
-  
+  };
+
   return (
     <MainPageLayout>
-      <input placeholder ="Type here something" p type ="text" onChange={onInputChange} onKeyDown ={onKeyDown} value ={input}/>
+      <input
+        placeholder="Type here something"
+        p
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
 
       <div>
-
         <label htmlFor="shows-search">
-
           Shows
-          <input id="shows-search" type="radio"  value="shows" onChange={onRadioChange} checked={isShowsSearch}/>
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            onChange={onRadioChange}
+            checked={isShowsSearch}
+          />
         </label>
         <label htmlFor="actors-search">
-
-            Actors
-          <input id="actors-search"type="radio" value="people" onChange={onRadioChange}  checked ={!isShowsSearch}/>
-          </label>
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            onChange={onRadioChange}
+            checked={!isShowsSearch}
+          />
+        </label>
       </div>
-      <button type ="button" onClick={onSearch}>Search</button>
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
       {renderResults()}
     </MainPageLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
